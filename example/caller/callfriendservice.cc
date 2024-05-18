20d1f046e7f9abd1b1e71b1c53cd9462eaf171cf
‘dwhdxp’ <dxp20001107@163.com>
2024-05-19 00:43:29 +0800

    example update increate controller

--- example/caller/callfriendservice.cc
+++ example/caller/callfriendservice.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "mpzrpcapplication.h"
-#include "mpzrpcchannel.h"
+#include "mpzrpccontroller.h"
 #include "friend.pb.h"
 
 int main(int argc, char** argv) {
@@ -12,16 +12,25 @@ int main(int argc, char** argv) {
     request.set_userid(100);
     fixbug::GetFriendListsResponse response;
 
-    stub.GetFriendLists(nullptr, &request, &response, nullptr);
-    if (response.result().errcode() == 0) {
-        std::cout << "rpc GetFriendLists Response success" << std::endl;
-        int n = response.friends_size();
-        for (int i = 0; i < n; ++i) {
-            std::cout << "index:" << i + 1 << " name: " << response.friends(i) << std::endl;
-        } 
+    // rpc调用状态信息，可以判断rpc调用是否成功以及输出失败信息
+    MpzrpcController controller;
+    
+    stub.GetFriendLists(&controller, &request, &response, nullptr);
+
+    if (controller.Failed()) {
+        std::cout << controller.ErrorText() << std::endl;
     }
     else {
-        std::cout << "rpc GetFriendLists Response error :" << response.result().errmsg() << std::endl;
+        if (response.result().errcode() == 0) {
+            std::cout << "rpc GetFriendLists Response success" << std::endl;
+            int n = response.friends_size();
+            for (int i = 0; i < n; ++i) {
+                std::cout << "index:" << i + 1 << " name: " << response.friends(i) << std::endl;
+            }
+        } 
+        else {
+            std::cout << "rpc GetFriendLists Response error: " << response.result().errmsg() << std::endl;
+        }
     }
 
     return 0;
